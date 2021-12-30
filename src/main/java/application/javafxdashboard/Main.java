2f8412dfb4a5e65861bb6d0048cc77f708c4138f
Detjon Mataj <84277348+detjonmataj@users.noreply.github.com>
2021-12-30 17:23:04 +0100

    Main application class loads initial scene

--- src/main/java/application/javafxdashboard/Main.java
+++ src/main/java/application/javafxdashboard/Main.java
@@ -1,29 +1,14 @@
 package application.javafxdashboard;
 
+import application.javafxdashboard.Controllers.SceneController;
 import javafx.application.Application;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        Text text = new Text("");
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(event -> text.setText("Hello World!"));
-        primaryStage.getIcons().add(new Image(String.valueOf(getClass().getResource("images/icon.png"))));
-        VBox box = new VBox(btn, text);
-        box.setAlignment(Pos.CENTER);
-        Scene root = new Scene(box, 300, 250);
-        primaryStage.setScene(root);
-        primaryStage.setResizable(false);
+        SceneController.LoadInitialScene(primaryStage);
         primaryStage.show();
     }
 
