7451789f2c864bc224343ed9248cd2c78a563bc4
Detjon <detjon.mataj@gmail.com>
2022-01-05 04:48:57 +0100

    Creating a main file which will run App class (old main)

--- src/main/java/application/javafxdashboard/Main.java
+++ src/main/java/application/javafxdashboard/Main.java
@@ -1,20 +1,7 @@
 package application.javafxdashboard;
 
-import application.javafxdashboard.Controllers.SceneController;
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) {
-        SceneController.LoadInitialScene(primaryStage);
-        primaryStage.getScene().getRoot().requestFocus();
-        primaryStage.show();
-    }
-
+public class Main {
     public static void main(String[] args) {
-        launch();
+        App.main(args);
     }
 }
